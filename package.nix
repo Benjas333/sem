c0aec54bf58995bcdaf124e85d706f6f5e596c63
Pipelight <127784580+pipelight@users.noreply.github.com>
2026-04-07 08:36:59 +0200

    Add Nix flake support. Thanks @pipelight
    * ci(nix): add base files for the nix package manager.
    
    * fix(nix): update paths to rust lock file

---
+++ package.nix
@@ -0,0 +1,45 @@
+{
+  pkgs ? import <nixpkgs> {},
+  lib,
+  ...
+}:
+pkgs.rustPlatform.buildRustPackage rec {
+  pname = "sem";
+  version = let
+    crate_name = pname + "-cli";
+  in
+    (builtins.fromTOML (lib.readFile "${src}/${crate_name}/Cargo.toml")).package.version;
+
+  src = ./crates;
+  cargoLock = {
+    lockFile = "${src}/Cargo.lock";
+    outputHashes = {
+      # "dummy-0.14.0" = lib.fakeHash;
+    };
+  };
+
+  # disable tests
+  checkType = "debug";
+  doCheck = false;
+
+  nativeBuildInputs = with pkgs; [
+    installShellFiles
+    pkg-config
+
+    llvmPackages.clang
+    clang
+  ];
+  buildInputs = with pkgs; [
+    openssl
+    pkg-config
+
+    (rust-bin.stable.latest.default)
+  ];
+
+  # postInstall = ''
+  #   installShellCompletion --cmd ${pname} \
+  #     --bash ./autocompletion/${pname}.bash \
+  #     --fish ./autocompletion/${pname}.fish \
+  #     --zsh  ./autocompletion/_${pname}
+  # '';
+}
